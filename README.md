2881081be7cd8fd6164e50fcfe3df309d5058ce3
MrTyme <flashcor96.dc@gmail.com>
2020-12-02 22:33:31 -0500

    CreateNote component was finished

---
+++ README.md
@@ -0,0 +1,66 @@
+Creacion App-Web de notas con MERN-stack.
+
+[X]--Creaciond del backend.
+    [X]--instalacion de modulos/dependencias.
+    [X]--creacion del puerto.
+    [X]--creacion de la base de datos.
+    [X]--conexion a la base de datos.
+    [X]--creacion del servidor.
+        [X]--creacion de las rutas.
+        [X]--funciones del servidor/controladores
+        [X]--modelacion de datos/models
+    
+
+[ ]--Creacion del frontend.
+    [X]--crear archivo react.
+    [ ]--Creacion de componentes.
+        [ ]--creacion de las vistas.
+            [X]--navegacion de vistas.
+            [X]--creacion de usuarios.
+            []--creacion de notas.
+            []--lista de notas.
+    [ ]--completacion del app.js
+
+
+
+//////////////////////////////////////
+
+**modulos a usar:
+
+cors: modulo que permite la conexion 
+entre servidores del la web y el del database
+
+dotenv: nos permite manipular variable de entorno.
+--con su modulo config va a ejecutar todas las 
+variables de entorno creadas.
+
+//////////////////////////////////////
+
+controladores:
+--permiten manipular de forma mas optima
+con archivos por separado los datos creados
+en la base de datos.
+
+rutas:
+--direccion en la que se desplazan los datos
+dentro de la base de datos
+
+//////////////////////////////////////
+
+--Atributos de react:
+
+path: se encarga de llamar a una direccion 
+de la url 
+
+exact: hace que la ruta coincida con la ruta que se
+envia por navegador.
+
+
+
+
+//////////////////////////////////////
+
+NOTA>> recuerda cuando vayas a crear
+una aplicacion completa tu solo
+debes crear dos carpetas fuente, 
+una para el backend y otra para el frontend.
--- frontend/src/components/CreateNote.js
+++ frontend/src/components/CreateNote.js
@@ -1,10 +1,127 @@
 import React, { Component } from 'react'
+import axios from 'axios';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+
 
 export default class CreateNote extends Component {
+    
+    //estado de los usuarios
+    state = {
+        users: [],
+        userSelected: '',
+        title:'',
+        content:'',
+        //propiedad para almacenar los estados
+        date: new Date(),
+    }
+
+    //llamado de los usuarios y enlistamiento de la nota
+    async componentDidMount(){
+        const res = await axios.get('http://localhost:4000/api/users');
+        this.setState({
+            users: res.data.map(user => user.username),
+            userSelected: res.data[0].username
+        });
+    }
+
+    //creacion de la nota
+    onSubmit = async (e) => {
+        e.preventDefault();
+        //esta constante va a enviar la nota al backend
+        const newNote = {
+            title: this.state.title,
+            content: this.state.content,
+            date: this.state.date,
+            author: this.state.userSelected
+        };
+        await axios.post('http://localhost:4000/api/notes', newNote);
+    
+        
+        //redirecion a la vista de notas:
+        window.location.href = '/';
+    }
+
+    //escucha la informacion del objeto
+    onInputChange = (e) => {
+        this.setState({
+            //usamos un array que por cada name me modifique el estado completo
+            [e.target.name]: e.target.value
+        });
+    }
+
+    //manipulacion de las fechas
+    onchangeDate = (date) => {
+        this.setState({date});
+    }
+
+    
     render() {
         return (
-            <div>
-                CREATE NOTE
+            <div className="col-md-6 offset-md-3">
+                <div className="card card-body">
+                    <h4>Create a note</h4>
+
+                    {/*SELECT USER*/}
+                    <div className="from-group">
+                        <select
+                            className="form-control"
+                            name="UserSelected"
+                            onChange = {this.onInputChange}
+                            >
+                            {
+                                //recorrido de los usuarios
+                                this.state.users.map(user => 
+                                    <option key = {user} value = {user}>
+                                        {user}
+                                    </option>
+                                )
+                            }
+                        </select>
+                    </div>
+
+                    {/*titulo de la nota a crear */}
+                    <div className="form-group">
+                        <input 
+                        type = "text" 
+                        className = "form-control" 
+                        placeholder="Title" 
+                        name = "title"
+                        onChange = {this.onInputChange}
+                        required
+                        />
+                    </div>
+                    
+
+                    {/*contenido de la nota */}
+                    <div className="form-group">
+                            <textarea
+                            name="content"
+                            className="form-control"
+                            placeholder="content"
+                            onChange = {this.onInputChange}
+                            required
+                            >
+
+                            </textarea>
+                    </div>
+
+                    {/*creacion de la fecha de la nota */}
+                    <div className="form-group">
+                        <DatePicker  
+                            className="form-control"
+                            selected={this.state.date}
+                            onChange={this.onchangeDate}
+                        />
+                    </div>
+
+
+                    <form onSubmit = {this.onSubmit}>
+                        <button type ="submit " className = "btn btn-primary">
+                            SAVE
+                        </button>
+                    </form>
+                </div>
             </div>
         )
     }
